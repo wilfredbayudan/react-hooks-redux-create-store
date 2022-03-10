3de5082b3bfe0a12deccec46931af012e36bbded
Wilfred <wilfredbayudan@gmail.com>
2022-03-09 20:35:12 -1000

    commit tree cb02dcd561981a5614d85aca0025debc031caa80

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,19 @@
-let state;
+function createStore(reducer) {
+  let state;
 
-function reducer(state = { count: 0 }, action) {
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return { dispatch, getState };
+};
+
+function counterReducer(state = { count: 0 }, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
@@ -8,21 +21,18 @@ function reducer(state = { count: 0 }, action) {
     default:
       return state;
   }
-}
+};
 
-function dispatch(action) {
-  state = reducer(state, action);
-  render();
-}
+let store = createStore(counterReducer);
 
 function render() {
   let container = document.getElementById("container");
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 }
 
-dispatch({ type: "@@INIT" });
+store.dispatch({ type: "@@INIT" });
 let button = document.getElementById("button");
 
 button.addEventListener("click", function () {
-  dispatch({ type: "counter/increment" });
+  store.dispatch({ type: "counter/increment" });
 });
